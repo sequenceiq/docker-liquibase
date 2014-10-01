3462a1b8849687f549982e1fb812696659ea7509
lpuskas <laszlo.puskas@sequenceiq.com>
2014-10-01 22:17:23 +0200

    Added debug msg

--- scripts/liquibase_update.sh
+++ scripts/liquibase_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 : ${CHANGELOG_FILE:="/changelogs/changelogs.xml"}
 
-echo "Applying changes to the database ...."
+echo "Applying changes to the database. Changelog: $CHANGELOG_FILE"
 liquibase --changeLogFile="/changelogs/$CHANGELOG_FILE" update
